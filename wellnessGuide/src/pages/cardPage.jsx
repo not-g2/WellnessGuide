7f5d468057de9ff806a54c72e4cae69f3698a0e7
Jittu Varghese <oommenjittu@gmail.com>
2025-10-04 17:10:07 +0530

    changed import

--- wellnessGuide/src/pages/cardPage.jsx
+++ wellnessGuide/src/pages/cardPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import ReactMarkdown from "react-markdown";
